328be0532b0a4beb835f0066567999db3960d992
Gerard Domenech <gerarddomenechdomingo@gmail.com>
2021-11-27 13:00:15 +0100

    2 first workshops added

---
+++ rover/pet_profile2/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello Roverines, you're at the pet profile2 index")
+
+
+
+
+    
